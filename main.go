11e2449a131dea42b90c17399c086421c6c4d10e
fajar2302 <fyasa1658@gmail.com>
2024-07-15 10:10:03 +0800

    add clean architecture

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// import (
+// 	"TokoGadget/internal/factory"
+
+// 	"github.com/labstack/echo/v4"
+// )
+
+// func main() {
+// 	e := echo.New()
+
+// 	factory.InitFactory(e)
+
+// 	e.Logger.Error(e.Start(":8000"))
+// }
